2f88fa7e8b7c7346ec7b3627b9b76a879b0c8445
tufeeqmushtaqnahami <tufeeqmushtaq000@gmail.com>
2026-08-20 19:38:13 +0530

    Fix TMDB API authentication

--- src/utils/Constants.jsx
+++ src/utils/Constants.jsx
@@ -4,17 +4,16 @@ export const USER_AVATAR =
 export const BG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/8cc08720-ac1c-4364-bcbd-9495bf0308cd/web/IN-en-20260323-TRIFECTA-perspective_0b8c8e4e-71ee-48bd-8e16-da74f083a838_large.jpg";
 
-
 export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-     process.env.REACT_APP_TMDB_KEY,
+    Authorization: `Bearer ${process.env.REACT_APP_TMDB_KEY}`,
   },
 };
 
-export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
+export const IMG_CDN_URL =
+  "https://image.tmdb.org/t/p/w500";
 
 export const OPEN_KEY =
-    process.env.REACT_APP_OPEN_KEY;
+  process.env.REACT_APP_OPEN_KEY;
